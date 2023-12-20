fd30ab4046af47680d5e4d33d444b116261ce346
Fa11enSky <133168352+Fa11enSky@users.noreply.github.com>
2023-12-20 13:52:27 +0200

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,36 @@
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+import ContactForm from './ContactForm/ContactForm';
+import { selectIsLoading } from 'store/selectors';
+import { useSelector } from 'react-redux';
+import Loader from './Loader/Loader';
+const App = () => {
+  const isLoading=useSelector(selectIsLoading)
+  return (
+    <div>
+      <h1
+        style={{
+          fontSize: '45px',
+          textAlign: 'center',
+          marginBottom: '20px',
+        }}
+      >
+        Phonebook
+      </h1>
+      <ContactForm />
+      <h2
+        style={{
+          fontSize: '40px',
+          marginBottom: '10px',
+          textAlign: 'center',
+        }}
+      >
+        Contacts
+      </h2>
+      <Filter />
+      {isLoading &&<Loader/>}
+      <ContactList />
+    </div>
+  );
+};
+export default App;
---
+++ src/store/store.js
@@ -0,0 +1,8 @@
+import {configureStore } from '@reduxjs/toolkit';
+
+import { contactReducer } from './contactSlice';
+import { filterReducer } from './filterSlice';
+
+export const store = configureStore({
+  reducer: { contacts: contactReducer, filter: filterReducer },
+});
